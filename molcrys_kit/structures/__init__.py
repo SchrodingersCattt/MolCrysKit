be339a1ed269554df6dfa03920e904e575887378
SchrodingersCattt <gmy721212@163.com>
2025-12-24 15:08:41 +0000

    fix imports

--- molcrys_kit/structures/__init__.py
+++ molcrys_kit/structures/__init__.py
@@ -5,7 +5,9 @@ This module contains the basic structural classes for representing atoms,
 molecules, and crystals.
 """
 
+from .atom import Atom
 from .molecule import CrystalMolecule
+from .crystal import MolecularCrystal
 
 # For backward compatibility
 Molecule = CrystalMolecule
