a406aeca9b788f60ca7b0b3e3bd91850862a6a31
SchrodingersCattt <gmy721212@163.com>
2025-12-29 07:51:53 +0000

    fix: remove redundant import in analysis module

--- molcrys_kit/analysis/__init__.py
+++ molcrys_kit/analysis/__init__.py
@@ -5,18 +5,10 @@ This module provides analysis capabilities for molecular crystals.
 """
 
 from .species import identify_molecules, assign_atoms_to_molecules
-from .interactions import (
-    Interaction,
-    detect_hydrogen_bonds,
-    detect_vdw_contacts,
-    analyze_interactions,
-)
+from .interactions import HydrogenBond
 
 __all__ = [
     "identify_molecules",
     "assign_atoms_to_molecules",
-    "Interaction",
-    "detect_hydrogen_bonds",
-    "detect_vdw_contacts",
-    "analyze_interactions",
+    "HydrogenBond"
 ]
