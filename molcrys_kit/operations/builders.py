7b98f1bcad7db40697849debb870fbce8ad98477
SchrodingersCattt <gmy721212@163.com>
2026-01-01 04:17:40 +0000

    fix create_supercell

--- molcrys_kit/operations/builders.py
+++ molcrys_kit/operations/builders.py
@@ -7,7 +7,6 @@ This module provides functionality to build complex structures from simpler unit
 import numpy as np
 from typing import Tuple
 
-from ase import Atoms
 
 from ..structures.crystal import MolecularCrystal
 
@@ -31,35 +30,18 @@ def create_supercell(
         Supercell structure.
     """
 
-    # Create ASE Atoms object from the crystal
-    symbols = []
-    positions = []
+    # Get the ASE Atoms object using the to_ase method
+    atoms = crystal.to_ase()
 
-    for mol in crystal.molecules:
-        mol_symbols = mol.get_chemical_symbols()
-        mol_positions = mol.get_positions()
-        symbols.extend(mol_symbols)
-        positions.extend(mol_positions)
-
-    # Create ASE Atoms object
-    atoms = Atoms(symbols=symbols, positions=positions, cell=crystal.lattice, pbc=True)
-
-    # Scale the cell by the given factors
-    scaled_cell = crystal.lattice.copy()
-    scaled_cell[0, :] *= m
-    scaled_cell[1, :] *= n
-    scaled_cell[2, :] *= p
-
-    # Create the supercell using ASE
-    atoms.set_cell(scaled_cell, scale_atoms=False)
-    supercell = atoms.repeat((m, n, p))
+    # Create the supercell using ASE's repeat method with the scaling factors
+    supercell = atoms.repeat(scaling_factors)
 
     # Rebuild the crystal structure from the supercell
     from ..io.cif import identify_molecules
 
     molecules = identify_molecules(supercell)
 
-    return MolecularCrystal(scaled_cell, molecules, crystal.pbc)
+    return MolecularCrystal(supercell.cell.copy(), molecules, crystal.pbc)
 
 
 def create_defect_structure(
