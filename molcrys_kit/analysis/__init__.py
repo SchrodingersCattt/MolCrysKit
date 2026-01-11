2852c5f1fdfb72ff87554fef70621dcf9c00e578
SchrodingersCattt <gmy721212@163.com>
2026-01-11 01:20:53 +0000

    remove redundant

--- molcrys_kit/analysis/__init__.py
+++ molcrys_kit/analysis/__init__.py
@@ -3,7 +3,7 @@ from .species import *
 from .stoichiometry import *
 from .chemical_env import ChemicalEnvironment
 
-# Import heuristics with a fallback for backward compatibility
+
 __all__ = [
     "ChemicalEnvironment"
 ]
